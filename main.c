bc513f289a2bf43614b268f7e15ddbc30abd6bcc
DishankBaid <dishankbaid9783@gmail.com>
2025-04-16 23:04:39 +0530

    Made a basic shell that executes the exit command

--- main.c
+++ main.c
@@ -1,38 +1,26 @@
 #include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
-#define LINE_BUFFER_SIZE 1024
+#define LINE_SIZE 1024
 
-void basic_loop()
+int main(int *argc, int argv)
 {
-    char *buffer;
-    size_t buffsize = 4;
-    size_t read;
+    char *line;
+    line = (char *)calloc(LINE_SIZE, sizeof(char));
 
-    buffer = (char *)malloc(buffsize * sizeof(char));
-
-    if (buffer == NULL)
+    while (1)
     {
-        perror("Unexpected Error occured and memory was not allocated.");
-        exit(EXIT_FAILURE);
+        line = readline(">");
+        printf("%s \n", line);
+
+        if (strcmp(line, "exit") == 0)
+        {
+            break;
+        }
+        free(line);
     }
 
-    printf("Type Something: ");
-    while (read = getline(&buffer, &buffsize, stdin))
-    {
-        printf("text: %s \n", buffer);
-    }
-
-    printf("Was not able to read line");
-    free(buffer);
-}
-
-int main()
-{
-    basic_loop();
     return 0;
 }
